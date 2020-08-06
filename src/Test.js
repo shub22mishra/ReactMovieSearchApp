8d08c41c73b25ecb7b469582e95124a42aa194e5
shub22mishra <47281314+shub22mishra@users.noreply.github.com>
2020-08-06 10:10:53 +0530

    first commit

---
+++ src/Test.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Test() {
+  return <h1>Hello world</h1>;
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
+import "./style.css";
+import SearchMovie from "./SearchMovie";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Main extends React.Component {
+  render() {
+    {
+      return (
+        <div className="container">
+          <h1 className="title"> React Movie Search</h1>
+          <SearchMovie />
+        </div>
+      );
+    }
+  }
+}
+ReactDom.render(<Main />, document.getElementById("root"));
+// API Key
+//4a0759204426c96159fccceea87f4d83
+// api request
+// https://api.themoviedb.org/3/movie/550?api_key=4a0759204426c96159fccceea87f4d83
+// API Read Access Token (v4 auth)
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YTA3NTkyMDQ0MjZjOTYxNTlmY2NjZWVhODdmNGQ4MyIsInN1YiI6IjVmMmEzNjA5NGU2NzQyMDAzNDc5Y2I0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5hs7LJK7rW_JDCbH4Ky9xJ3-5tiAMuJiTmw-hGKLUMk
